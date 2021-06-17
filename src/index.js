2177ab1ac8a50f4e211a39841b078324928dd569
Erica Winne <erica.winne@powerhrg.com>
2021-06-17 10:04:01 -0400

    persistance updates

--- src/index.js
+++ src/index.js
@@ -1,36 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const objectIDs = [437984, 436532, 436535]
+  const paintingIds = [437984, 436532, 436535]
   const paintingList = document.querySelector("#painting-list")
-  showAllPaintings(objectIDs)
-  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectIDs[0]}`
+  showAllPaintings(paintingIds)
+  const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${paintingIds[0]}`
   fetch(url)
     .then(resp => resp.json())
     .then(data => {
-      console.log(data)
       vanGoghArtDetails(data)
     })
 
   function showAllPaintings (paintingIds) {
-    const paintingThumbnails = document.querySelector("#paintings")
+    const paintingThumbnailContainer = document.querySelector("#paintings")
     paintingIds.forEach(id => {
-    console.log(`${id} look here`)
       const url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/${id}`
       fetch(url)
         .then(resp => resp.json())
         .then(data => {
-          console.log(data.primaryImageSmall)
-          const img = document.createElement("img")
-          img.src = data.primaryImageSmall
-          img.addEventListener("click", () => {
-              vanGoghArtDetails(data)
+          const thumbnail = document.createElement("img")
+          thumbnail.src = data.primaryImageSmall
+          paintingThumbnailContainer.append(thumbnail)
+          thumbnail.addEventListener("click", () => {
+            vanGoghArtDetails(data)
           })
-          paintingThumbnails.append(img)
         })
     })
-    console.log(paintingIds)
   }
+  const commentBox = document.querySelector("textarea")
+  const submitBtn = document.querySelector("#submitButton")
+  submitBtn.addEventListener("click", (event) => {
+    event.preventDefault()
+    // const li = document.createElement("li")
+    // li.innerHTML = commentBox.value
+    // commentUl.append(li)
+    // commentBox.value = ""
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        content: commentBox.value,
+        artId: commentBox.id
+      })
+    }
+    const url = "http://localhost:3000/comments/"
+    fetch(url, configurationObject)
+  })
 
   function vanGoghArtDetails (art) {
+    commentBox.id = art.objectID
     paintingList.innerHTML = ""
 
     const title = document.querySelector("h2")
@@ -50,16 +69,18 @@ document.addEventListener("DOMContentLoaded", () => {
     const artCreditLineLi = document.createElement("li")
     artCreditLineLi.innerHTML = art.creditLine
     paintingList.append(artCreditLineLi)
+    const commentUl = document.querySelector(".commentsSaved")
+    fetch("http://localhost:3000/comments")
+      .then(resp => resp.json())
+      .then(data => {
+        const paintingComments = data.filter(element => element.artId === art.objectID)
+        console.log(paintingComments)
+        console.log(art.objectID)
+      })
   }
-
-  const commentBox = document.querySelector("textarea")
-  const submitBtn = document.querySelector("#submitButton")
-  const commentUl = document.querySelector(".commentsSaved")
-  submitBtn.addEventListener("click", (event) => {
-    event.preventDefault()
-    const li = document.createElement("li")
-    li.innerHTML = commentBox.value
-    commentUl.append(li)
-    commentBox.value = ""
-  })
 })
+
+/* -when a user leaves a comment on a specific painting, we are going to pathc
+ the users comment to the corrosponding painting array
+
+ */
